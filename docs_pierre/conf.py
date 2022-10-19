9cc246487061663401d0518541056192ca288778
pchaumeil <uqpchaum@uq.edu.au>
2022-10-20 08:12:02 +1000

    feat(Tools implementation): Add first set of tools
    - Checksum
    - Seq_io like
    - symlink
    
    [skip ci]

---
+++ docs_pierre/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+import sphinx_rtd_theme
+sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
+sys.path.insert(0, os.path.abspath('../gtdblib'))
+sys.path.insert(0, os.path.abspath('../gtdblib/util'))
+sys.path.insert(0, os.path.abspath('../gtdblib/util/shell'))
+sys.path.insert(0, os.path.abspath('../gtdblib/util/bio'))
+
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'GTDB Library'
+copyright = '2022, Aaron,Donovan,Pierre'
+author = 'Aaron,Donovan,Pierre'
+release = '0.0.1'
+
+
+
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx_rtd_theme",
+              "sphinx.ext.autodoc"]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ['_static']
+
--- gtdblib/util/color.py
+++
@@ -1,2 +0,0 @@
-def rgb_to_hex(r: int, g: int, b: int) -> str:
-    return f'#{r:X}{g:X}{b:X}'
