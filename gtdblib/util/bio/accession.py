eb7bd1619c15c36e8a9f26b880a8e391f03b3992
dparks1134 <donovan.parks@gmail.com>
2023-01-08 03:23:33 +1000

    feat: added execute.py and method for checking accession versions in accession.py.

--- gtdblib/util/bio/accession.py
+++ gtdblib/util/bio/accession.py
@@ -11,7 +11,10 @@ def canonical_gid(gid: str) -> str:
         GCF_005435135.1 -> G005435135
         GCF_005435135.1_ASM543513v1_genomic -> G005435135
         RS_GCF_005435135.1 -> G005435135
-        GB_GCA_005435135.1 -> G005435135
+        GB_GCA_005435135.1 ->
+
+    :param gid: Genome accesion to conver to canonical form.
+    :return: Canonical form of accession.
     """
 
     match = RE_CANONICAL.match(gid)
@@ -19,3 +22,18 @@ def canonical_gid(gid: str) -> str:
         return f'G{match[1]}'
     else:
         return gid
+
+
+def is_same_accn_version(accn1: str, accn2: str) -> bool:
+    """Check if accessions have same version number.
+
+    This method assumes accession versions are provided
+    as a suffix proceeding a period. This is the format
+    used for NCBI accessions, e.g. GCF_005435135.1.
+
+    :param accn1: First accession.
+    :param accn2: Second accession.
+    :return: True if version number is the same, else False.
+    """
+
+    return int(accn1.split('.')[1]) == int(accn2.split('.')[1])
