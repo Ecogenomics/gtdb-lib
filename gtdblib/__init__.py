58c129dde73bbb4bdee9813eea1d43dd6222f9a0
Aaron Mussig <aaronmussig@gmail.com>
2023-04-13 15:03:53 +1000

    build(poetry): Move to poetry package manager.

--- gtdblib/__init__.py
+++ gtdblib/__init__.py
@@ -1,13 +1,4 @@
-__title__ = 'gtdblib'
-__description__ = 'An abstraction of objects, files, and third-party tools used by the GTDB. Not intended for public use.'
-__url__ = 'https://github.com/Ecogenomics/gtdb-lib'
 __version__ = '1.9.0'
-__author__ = 'Aaron Mussig'
-__author_email__ = 'aaronmussig@gmail.com'
-__license__ = 'GPL-3.0'
-__bug_url__ = 'https://github.com/Ecogenomics/gtdb-lib'
-__doc_url__ = 'https://github.com/Ecogenomics/gtdb-lib'
-__src_url__ = 'https://github.com/Ecogenomics/gtdb-lib'
 
 import logging
 
